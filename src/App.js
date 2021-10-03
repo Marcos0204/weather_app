bb2029e83bdef3957e85fd4b032e3bbcf149116d
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-03 01:11:52 -0500

    input-structure

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { fetchWeather } from './api/fetchWeather';
 import './App.css'
 
 const App = () => {
-    //const [ query, setQuery ] = useState('')
+    const [ query, setQuery ] = useState('')
     return (
         <div className='main-container'>
-            
+            <input
+                type='text'
+                className='search'
+                placeholder='Search...'
+                value={query}
+                onChange= {(e) => setQuery(e.target.value)}
+            />
         </div>
     )
 }
