76b210ee745f7a0117e37179ca2c57a745c1d088
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-10-03 01:25:45 -0500

    making request and saving the information

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import { fetchWeather } from './api/fetchWeather';
-import './App.css'
+import './App.css';
 
 const App = () => {
-    const [ query, setQuery ] = useState('')
+    const [ query, setQuery ] = useState('');
+    const [ weather, setWeather ] = useState({})
+    const Search = async (e) =>{
+        if(e.key === 'Enter'){
+            const data = await fetchWeather(query)
+            setWeather(data);
+            setQuery('');
+        }
+    }
+    console.log(weather)
     return (
         <div className='main-container'>
             <input
@@ -12,6 +21,7 @@ const App = () => {
                 placeholder='Search...'
                 value={query}
                 onChange= {(e) => setQuery(e.target.value)}
+                onKeyPress={Search}
             />
         </div>
     )
